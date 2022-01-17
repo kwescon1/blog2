d8b10452400396852f9df2a4394a4ad10a6590a4
Odame <kwesi.danquah@daaseng.com>
2022-01-17 18:33:10 +0000

    remove dummy text

--- resources/views/frontend/layout/partials/news_letter.blade.php
+++ resources/views/frontend/layout/partials/news_letter.blade.php
@@ -4,8 +4,8 @@
             <div class="col-md-5">
                 <div class="subscribe-1 ">
                     <h2>Subscribe to our newsletter</h2>
-                    <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit nesciunt
-                        error illum a explicabo, ipsam nostrum.</p>
+                    {{-- <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit nesciunt --}}
+                    {{-- error illum a explicabo, ipsam nostrum.</p> --}}
                     <form action="#" class="d-flex">
                         <input type="text" class="form-control" placeholder="Enter your email address">
                         <input type="submit" class="btn btn-primary" value="Subscribe">
