f2add7eb6dbdf436067281ef9632badadf69344c
Odame <kwesi.danquah@daaseng.com>
2022-02-08 17:43:48 +0000

    add description meta

--- resources/views/frontend/detail.blade.php
+++ resources/views/frontend/detail.blade.php
@@ -6,7 +6,7 @@
     {{ $tag->name }},
     @endforeach">
     <meta name="author" content="{{ $post->user->name }}">
-    {{-- <meta name="description" content="{!! \Illuminate\Support\Str::limit($post->content, 40, '...') !!}"> --}}
+    <meta name="description" content="{!! \Illuminate\Support\Str::limit($post->content, 40, '...') !!}">
 
     <meta property="og:image" content="{{ asset($post->image800x549) }}" />
     <meta property="og:image:secure_url" content="{{ asset($post->image800x549) }}" />
