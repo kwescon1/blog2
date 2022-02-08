b3e21f5fdccd91920b424038b5d3267abc405bf4
Odame <kwesi.danquah@daaseng.com>
2022-02-08 16:21:31 +0000

    string limit content for meta tag

--- resources/views/frontend/detail.blade.php
+++ resources/views/frontend/detail.blade.php
@@ -1,27 +1,16 @@
 @extends('frontend.layout.app_layout')
 
 @section('meta')
-    <meta name="keywords" content="                                      @foreach ($post->tags as $tag)
+    <meta name="keywords" content="                                        @foreach ($post->tags as $tag)
     {{ $tag->name }},
     @endforeach">
     <meta name="author" content="{{ $post->user->name }}">
-    <meta name="description" content="{{ $post->title }}">
+    <meta name="description" content="{!! str_limit($post->content, 40, '...') !!}">
 
     <meta property="og:image" content="{{ asset($post->image800x549) }}" />
     <meta property="og:image:secure_url" content="{{ asset($post->image800x549) }}" />
 
     <!-- Twitter -->
-
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:site" content="@nytimesbits" />
-    <meta name="twitter:creator" content="@nickbilton" />
-    <meta property="og:url" content="http://bits.blogs.nytimes.com/2011/12/08/a-twitter-for-my-sister/" />
-    <meta property="og:title" content="A Twitter for My Sister" />
-    <meta property="og:description"
-        content="In the early days, Twitter grew so quickly that it was almost impossible to add new features because engineers spent their time trying to keep the rocket ship from stalling." />
-    <meta property="og:image"
-        content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg" />
-
     {{-- <meta name="twitter:card" content="summary_large_image"> --}}
     {{-- </meta> --}}
     {{-- <meta name="twitter:site" content="@themepixels"> --}}
