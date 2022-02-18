ec784d2cb82aaf44cc8478332029203f7280ff27
Odame <kwesi.danquah@daaseng.com>
2022-02-18 16:11:38 +0000

    cache in different names

--- app/Modules/Post/Services/PostService.php
+++ app/Modules/Post/Services/PostService.php
@@ -250,13 +250,13 @@ class PostService extends CoreService implements PostServiceInterface
     public function getFirstFivePosts(): ?Collection
     {
         //cache
-        return $this->cachePost('homePage', 60 * 24 * 60, $this->postRepository->getFirstFivePosts(self::STATUS_PUBLISHED));
+        return $this->cachePost('firstFive', 60 * 24 * 60, $this->postRepository->getFirstFivePosts(self::STATUS_PUBLISHED));
     }
 
     public function getLastFourPosts(): ?Collection
     {
         //cache
-        return $this->cachePost('homePage', 60 * 24 * 60, $this->postRepository->getLastFourPosts(self::STATUS_PUBLISHED));
+        return $this->cachePost('lastFour', 60 * 24 * 60, $this->postRepository->getLastFourPosts(self::STATUS_PUBLISHED));
     }
 
     private function cachePost($cacheKey, $duration, $query)
