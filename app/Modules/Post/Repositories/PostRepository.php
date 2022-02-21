42b3862592f065f5180fd0ec5c2f8ba23715ced7
Odame <kwesi.danquah@daaseng.com>
2022-02-21 22:29:24 +0000

    fix related posts

--- app/Modules/Post/Repositories/PostRepository.php
+++ app/Modules/Post/Repositories/PostRepository.php
@@ -86,6 +86,8 @@ class PostRepository extends BaseRepository implements PostRepositoryInterface
     {
         return $this->model()->inRandomOrder()->with('category')->where('status', $status)->where('slug', '!=', $post->slug)->whereHas('tags', function ($q) use ($post) {
             $q->whereIn('name', $post->tags->pluck('name'));
+        })->orWhereHas('category', function ($w) use ($post) {
+            $w->where('name', $post->category->name);
         })->limit(4)->get();
 
         //try where cateory
